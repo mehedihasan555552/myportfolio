d04e73aa319324d46932a2af2ebafded968cffe0
Mehedi Hasan <mehedihasan555552@gmail.com>
2021-08-10 13:44:20 +0600

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import Home from './components/Home'
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 
 
 function App() {
   return (
     <div>
       <Home/>
-      <MessengerCustomerChat
-      pageId="1965283286842649"        
-      appId="376127083909737"
-               
-                 />
-
-    </div>
+    
   );
 }
 
